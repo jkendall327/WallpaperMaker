d165874fd7bd63064faae5fe3d6dfa89d6ba47c3
Jack Kendall <jkendall3096@gmail.com>
2021-06-14 16:08:41 +0100

    Port old code, integrate ImageSharp

--- WallpaperMaker/Pages/ViewImage.cshtml.cs
+++ WallpaperMaker/Pages/ViewImage.cshtml.cs
@@ -1,13 +1,10 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.Drawing;
 
 namespace WallpaperMaker.Pages
 {
     public class ViewImageModel : PageModel
     {
         public string ImagePath { get; set; }
-
-        public Image Img { get; set; }
 
         public void OnGet(string path)
         {
